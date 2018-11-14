26d8ee68fd2ad6fa4ae2ac32ce28208f5e426598
JordyErick <41696650+JordyErick@users.noreply.github.com>
2018-11-14 11:59:09 -0500

    Update Encolamiento.sql

--- Encolamiento.sql
+++ Encolamiento.sql
@@ -1,8 +1,8 @@
 select tb.client, sum(tb.encolados)  from
 (select cl.client,
-		( select count(*) from usrsms.outgoing_message o where o.campaign_id = s.id and estado = 1 ) as encolados 
+	( select count(*) from usrsms.outgoing_message o where o.campaign_id = s.id and estado = 1 ) as encolados 
 from usrsms.corporates cp INNER JOIN usrsms.client cl on cp.client_id = cl.id
-												 INNER JOIN usrsms.sendings s on cp.username = s.corporate_id
+INNER JOIN usrsms.sendings s on cp.username = s.corporate_id
 where date_trunc('day', s.scheduled_start_date) >= CURRENT_DATE
 or date_trunc('day', s.creation_date) >= CURRENT_DATE ) as tb
 GROUP BY tb.client
