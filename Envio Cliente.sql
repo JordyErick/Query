3dbe6fa7b2daf17b44affa6418dca7f9b8fae931
JordyErick <41696650+JordyErick@users.noreply.github.com>
2018-11-21 16:59:45 -0500

    Update Envio Cliente.sql

--- Envio Cliente.sql
+++ Envio Cliente.sql
@@ -2,3 +2,10 @@ select s.corporate_id, sum(s.total_messages) from usrsms.sendings s
 where s.corporate_id in( select username from usrsms.corporates  where client_id=7)
 and date_trunc('day', s.scheduled_start_date) BETWEEN CURRENT_DATE -29 and CURRENT_DATE
 GROUP BY s.corporate_id
+
+
+
+-- ENVIO DE MES 
+select SUM(s.total_messages) from usrsms.sendings s where (date_trunc ('day',s.scheduled_start_date)  BETWEEN '2018-08-01' and '2018-08-31'
+        or date_trunc('day', s.scheduled_start_date )  BETWEEN'2018-08-01' and  '2018-08-31')
+
