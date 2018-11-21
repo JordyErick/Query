46fe22d3ccee76c1a7a17d1de363285353306303
JordyErick <41696650+JordyErick@users.noreply.github.com>
2018-11-21 17:07:14 -0500

    Update Envio Cliente.sql

--- Envio Cliente.sql
+++ Envio Cliente.sql
@@ -5,10 +5,15 @@ and date_trunc('day', s.scheduled_start_date) BETWEEN CURRENT_DATE -29 and CURRE
 GROUP BY s.corporate_id
 
 
+--ENVIO DEL DIA
+select SUM(s.total_messages) from usrsms.sendings s where (date_trunc ('day',s.scheduled_start_date) = CURRENT_DATE 
+        or date_trunc('day', s.scheduled_start_date) = CURRENT_DATE ) and s.process_status=7
+
 
 -- ENVIO DE MES 
 select SUM(s.total_messages) from usrsms.sendings s where (date_trunc ('day',s.scheduled_start_date)  BETWEEN '2018-08-01' and '2018-08-31'
         or date_trunc('day', s.scheduled_start_date )  BETWEEN'2018-08-01' and  '2018-08-31')
+        
         
 --ENVIO DE PITCH        
 select count(*) from usrsms.outgoing_message where fecha_envio < '2018-08-28 11:00' 
